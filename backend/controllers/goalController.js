a46258eaa630595e7829daa41e364d4a2762078b
re-gis <irumvaregisdmc@gmail.com>
2023-01-18 13:44:17 +0200

    MongoDB connection

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,13 +1,15 @@
 const asynHandler = require('express-async-handler');
+const { json } = require("express");
+const Goal = require('../models/goalModel');
+
 
 //Desc Get goals
 //Route GET/api/goals
-
-const { json } = require("express");
-
 //Access private
 const getGoals = asynHandler( async (req, res) => {
-    res.status(200).json({message: 'Get goal'});
+const goals = await Goal.find();
+
+    res.status(200).json(goals);
 });
 
 
@@ -19,8 +21,12 @@ const setGoal = asynHandler( async (req, res) => {
         res.status(400)
         throw new Error('Please add a text field');
     };
+const goal = await Goal.create({
+    text: req.body.text,
+})
 
-    res.status(200).json({message: 'Set goal'});
+
+    res.status(200).json(goal);
 });
 
 
@@ -28,7 +34,17 @@ const setGoal = asynHandler( async (req, res) => {
 //Route PUT/api/goals/:id
 //Access private
 const updateGoal = asynHandler( async (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`});
+    const goal = await Goal.findById(req.params.id);
+    if(!goal) {
+        res.status(400);
+        throw new Error('Goal not found!');
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    });
+
+
+    res.status(200).json(updatedGoal);
 });
 
 
@@ -36,7 +52,16 @@ const updateGoal = asynHandler( async (req, res) => {
 //Route DELETE/api/goals/:id
 //Access private
 const deleteGoal = asynHandler( async (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`});
+    const goal = await Goal.findById(req.params.id);
+    if(!Goal) {
+        res.status(400);
+        throw new Error('Goal not found!');
+    }
+
+    await goal.remove()
+
+
+    res.status(200).json({id: req.params.id});
 });
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const { json } = require('express');
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
-const { errorHandler } = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware');
+const { default: mongoose } = require('mongoose');
 
-
+mongoose.set('strictQuery',true);
+connectDB();
 
 const app = express();
 
